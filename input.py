d6eb46307609cad808d4ae7c2c8f84ad5895a048
KyleNi <kylejessie27@gmail.com>
2025-12-05 00:03:05 +0800

    Create input.py

---
+++ input.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+
+
+age = input("Enter your age: ")
+age = int(age)   # convert to number
+print("Next year you will be", age + 1)
+
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
