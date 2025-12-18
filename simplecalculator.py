68f5ffa3b636c4c841378f81c6afb0849ede7fba
KyleNi <kylejessie27@gmail.com>
2025-12-18 09:42:57 +0800

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,5 +1,12 @@
-num1 = int(input("Enter first number"))
-num2 = int(inout("Enter second number"))
-operation = input("Enter operation(+,-,*,/): ")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter first number: "))
+operation =input("Enter operation(+,-,*,/): ")
+
 if operation == '+':
-   reault = num1 + num2
+   result = num1 + num2
+   elif operation == '-':
+   result = num1 - num2
+   elif operation == '*':
+   result = num1 * num2
+   else operation == '/':
+       operation != 0:
